e29e0a083e83b332fb4d99a410e5bf35b5bb6344
Tim Tavlintsev <ttavlintsev@enttec.com>
2022-09-01 22:39:44 +1000

    Up readme

--- README.md
+++ README.md
@@ -23,6 +23,19 @@ make
 ## Usage 
 - in Cmake file add:
 `target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_SOURCE_DIR}/include/mdnsServicepp)`
-- or just #include "mdnsServise.h"
+- or just #include "mdnsService.h"
 
-Class MdnsServise implements mdns.c logic with minimal changes so user can rely on https://github.com/mjansson/mdns documentation
+Class MdnsService implements mdns.c logic with minimal changes so user can rely on https://github.com/mjansson/mdns documentation
+
+MdnsService accepts logging function for custom loggers e.g. 
+```
+mdns::MdnsService service([](const std::string &str) { PLOGD << str; });
+``` 
+by default used:
+```
+std::function<void(const std::string &)> logger_callback = [](const std::string &str) {
+    static std::mutex logLock;
+    std::lock_guard<std::mutex> lock(logLock);
+    std::cout << str << std::endl;
+};
+```
--- example.cpp
+++ example.cpp
@@ -5,10 +5,16 @@ main()
 {
     mdns::MdnsService service;
     auto txts = mdns::TxtRecordArray{std::make_pair("mac", "13:13:13:13:13:13"),
-                                     std::make_pair("enttec", "S-Play asdasdjsa;ldj alkdsjlsa djsalkdjsakld")};
-    service.start("_http._tcp.local.", "Hello", txts);
+                                     std::make_pair("rand", "asdaood asdasdjsa;ldj alkdsjlsa djsalkdjsakld")};
+    
+    service.start("_http._tcp.local.", "Hello", std::move(txts));
+    
+    // DNS-SD discovery
     service.discover();
+
+    // Find services with name "_http._tcp.local."
     auto discovered = service.sendMdnsQuery("_http._tcp.local.");
+
     for (const auto &res : discovered) {
         std::cout << "MdnsQuery Result: " << mdns::ipv4_address_to_string(&res.ipv4) << " service=" << res.service
                   << " hostname=" << res.hostname << " txt records: ";
@@ -18,7 +24,7 @@ main()
     }
 
     while (1) {
-        ;
+        ; // run for self discovery
     }
     exit(0);
 }
